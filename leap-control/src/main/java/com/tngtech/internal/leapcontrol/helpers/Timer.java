cdc1d3ecb84b57b770711112ff14c6772cad637f
Thomas Endres <Thomas-Endres@gmx.de>
2013-05-28 17:02:54 +0200

    Integrated timer component

---
+++ leap-control/src/main/java/com/tngtech/internal/leapcontrol/helpers/Timer.java
@@ -0,0 +1,35 @@
+package com.tngtech.internal.leapcontrol.helpers;
+
+import java.util.Date;
+
+public class Timer
+{
+  private Date startTime;
+
+  private Date endTime;
+
+  public void start()
+  {
+    startTime = new Date();
+    endTime = null;
+  }
+
+  public void stop()
+  {
+    endTime = new Date();
+  }
+
+  public long getElapsedSeconds()
+  {
+    if (startTime == null)
+    {
+      return 0;
+    } else if (endTime == null)
+    {
+      return new Date().getTime() - startTime.getTime();
+    } else
+    {
+      return endTime.getTime() - startTime.getTime();
+    }
+  }
+}
