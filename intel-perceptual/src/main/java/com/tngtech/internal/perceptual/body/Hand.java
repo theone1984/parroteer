24f84bb39c6eba1fce105d9981ca9a571c347a9d
Martin Förtsch <martin.foertsch@tngtech.com>
2013-06-22 17:55:13 +0200

    Added new abstraction class for Hands due to bad c-style sdk

---
+++ intel-perceptual/src/main/java/com/tngtech/internal/perceptual/body/Hand.java
@@ -0,0 +1,45 @@
+package com.tngtech.internal.perceptual.body;
+
+import intel.pcsdk.PXCMGesture.GeoNode;
+import intel.pcsdk.PXCMPoint3DF32;
+
+public class Hand {
+	
+	private GeoNode geoData;
+	
+	public Hand(GeoNode geoNode) {
+		this.geoData = geoNode;
+	}
+	
+	public float getX() {
+		if ( isActive() ) {
+			return getPositionWorld().x;
+		}
+		
+		return 0;
+	}
+	
+	public float getY() {
+		if ( isActive() ) {
+			return getPositionWorld().z;
+		}
+		
+		return 0;
+	}
+	
+	public float getZ() {
+		if ( isActive() ) {
+			return getPositionWorld().y;			
+		}
+		
+		return 0;
+	}
+
+	public boolean isActive() {
+		return this.geoData.positionWorld != null;
+	}
+	
+	private PXCMPoint3DF32 getPositionWorld() {
+		return this.geoData.positionWorld;
+	}
+}
