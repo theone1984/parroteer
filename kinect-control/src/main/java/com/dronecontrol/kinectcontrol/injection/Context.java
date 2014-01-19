2a5d827194e2d592d63e2f2a8ae937b1a2bdd5e1
Thomas Endres <Thomas-Endres@gmx.de>
2014-01-19 15:14:03 +0100

    Integrated config builder

--- kinect-control/src/main/java/com/dronecontrol/kinectcontrol/injection/Context.java
+++ kinect-control/src/main/java/com/dronecontrol/kinectcontrol/injection/Context.java
@@ -1,5 +1,6 @@
 package com.dronecontrol.kinectcontrol.injection;
 
+import com.dronecontrol.kinectcontrol.config.Config;
 import com.dronecontrol.kinectcontrol.input.KinectController;
 import com.dronecontrol.kinectcontrol.input.KinectDataReceiver;
 import com.google.inject.AbstractModule;
@@ -11,6 +12,7 @@ import com.dronecontrol.kinectcontrol.control.DroneInputController;
 import com.dronecontrol.kinectcontrol.entry.Main;
 import com.dronecontrol.kinectcontrol.helpers.RaceTimer;
 import com.dronecontrol.kinectcontrol.ui.FxWindow;
+import com.tngtech.configbuilder.ConfigBuilder;
 
 public class Context extends AbstractModule
 {
@@ -35,15 +37,34 @@ public class Context extends AbstractModule
   @Override
   protected void configure()
   {
+    bindBeansToImplementations();
+    bindBeansToScope();
+    bindBeansToProviders();
+  }
+
+  private void bindBeansToImplementations()
+  {
+    bind(Config.class).toInstance(getConfig());
+  }
+
+  private void bindBeansToScope()
+  {
     bind(Main.class).in(Singleton.class);
     bind(DroneInputController.class).in(Singleton.class);
     bind(FxWindow.class).in(Singleton.class);
 
-    bind(DroneController.class).toProvider(DroneControllerProvider.class);
-
     bind(KinectController.class).in(Singleton.class);
     bind(KinectDataReceiver.class).in(Singleton.class);
-
     bind(RaceTimer.class).in(Singleton.class);
+  }
+
+  private void bindBeansToProviders()
+  {
+    bind(DroneController.class).toProvider(DroneControllerProvider.class);
+  }
+
+  private Config getConfig()
+  {
+    return new ConfigBuilder<>(Config.class).build();
   }
 }
--- kinect-control/src/main/java/com/dronecontrol/kinectcontrol/input/KinectController.java
+++ kinect-control/src/main/java/com/dronecontrol/kinectcontrol/input/KinectController.java
@@ -1,5 +1,6 @@
 package com.dronecontrol.kinectcontrol.input;
 
+import com.dronecontrol.kinectcontrol.config.Config;
 import com.dronecontrol.kinectcontrol.input.events.MovementDataListener;
 import com.dronecontrol.kinectcontrol.input.socket.SocketClient;
 
@@ -7,21 +8,22 @@ import javax.inject.Inject;
 
 public class KinectController
 {
-  private static final String HOST_NAME = "localhost";
-
-  private static final int HOST_PORT = 4500;
-
   private final SocketClient socketClient;
 
   private final KinectDataReceiver kinectDataReceiver;
 
+  private final String hostName;
+
+  private final Integer port;
+
   @Inject
-  public KinectController(SocketClient socketClient, KinectDataReceiver kinectDataReceiver)
+  public KinectController(Config config, SocketClient socketClient, KinectDataReceiver kinectDataReceiver)
   {
     this.socketClient = socketClient;
     this.kinectDataReceiver = kinectDataReceiver;
 
-
+    hostName = config.getHostName();
+    port = config.getPort();
   }
 
   public void addMovementDataListener(MovementDataListener droneInputController)
@@ -31,7 +33,7 @@ public class KinectController
 
   public void start()
   {
-    socketClient.connect(HOST_NAME, HOST_PORT);
+    socketClient.connect(hostName, port);
     socketClient.addDataListener(kinectDataReceiver);
   }
 
